a104bc0806d5872e9a4033882daba94565c6cee3
Anton Kirsanov <antonkirsanov168@gmail.ru>
2026-03-16 23:25:07 +0300

    added homework1

---
+++ src/homework1/Homework1.java
@@ -0,0 +1,10 @@
+package homework1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int b=10;
+        int c=8;
+        int a=4*(b+c-1)/2;
+        System.out.println(a);
+    }
+}
---
+++ src/homework1/Homework3.java
@@ -0,0 +1,13 @@
+package homework1;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        int n=124;
+       int a=n/100;
+       int b=n%100/10;
+       int c=n%10;
+       int summ=a+b+c;
+       System.out.println(summ);
+
+    }
+}
